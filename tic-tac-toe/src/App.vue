0ecbbe7e42567ac82f05c2bbc1ffa1c5ee56d337
nmadhab <nilmadhab.mondal@booking.com>
2021-11-05 09:55:28 +0100

    game logic implemented

--- tic-tac-toe/src/App.vue
+++ tic-tac-toe/src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <!--  https://dev.to/ayushmanbthakur/how-to-make-tic-tac-toe-in-browser-with-html-css-and-js-28ed-->
   <div class="container">
     <h1>Tic-Tac-Toe</h1>
     <div class="play-area">
-      <div id="block_0" class="block">X</div>
-      <div id="block_1" class="block">O</div>
-      <div id="block_2" class="block"></div>
-      <div id="block_3" class="block"></div>
-      <div id="block_4" class="block"></div>
-      <div id="block_5" class="block"></div>
-      <div id="block_6" class="block"></div>
-      <div id="block_7" class="block"></div>
-      <div id="block_8" class="block"></div>
+      <div id="block_0" class="block" @click="draw(0)">{{ content[0] }}</div>
+      <div id="block_1" class="block" @click="draw(1)">{{ content[1] }}</div>
+      <div id="block_2" class="block" @click="draw(2)">{{ content[2] }}</div>
+      <div id="block_3" class="block" @click="draw(3)">{{ content[3] }}</div>
+      <div id="block_4" class="block" @click="draw(4)">{{ content[4] }}</div>
+      <div id="block_5" class="block" @click="draw(5)">{{ content[5] }}</div>
+      <div id="block_6" class="block" @click="draw(6)">{{ content[6] }}</div>
+      <div id="block_7" class="block" @click="draw(7)">{{ content[7] }}</div>
+      <div id="block_8" class="block" @click="draw(8)">{{ content[8] }}</div>
     </div>
+    <h2 id="winner" v-if="isOver"> Winner is {{winner}} </h2>
+    <h2 v-if="isTie"> Game is Tie</h2>
+    <!--    Reset board-->
+    <button @click="resetBoard()" v-if="isOver || isTie">RESET BOARD</button>
   </div>
 </template>
 
@@ -25,9 +28,58 @@
       return {
         content: ["", "", "", "", "", "", "", "", ""],
         turn: true,
+        isOver: false,
+        winner: null,
+        isTie: false
       }
     },
-    methods: {}
+    methods: {
+      draw(index) {
+        // send event to socket.io
+        if(this.turn) {
+          // if turn is true then mark as X or mark as O
+          this.content[index] = "X"
+        } else {
+          this.content[index] = "O"
+        }
+        this.turn = !this.turn;
+        // calculate the winner
+        this.calculateWinner();
+        this.calculateTie()
+      },
+      calculateWinner() {
+        const WIN_CONDITIONS = [
+          // rows
+          [0, 1, 2], [3, 4, 5], [6, 7, 8],
+          // cols
+          [0, 3, 6], [1, 4, 7], [2, 5, 8],
+
+          // diagonals
+
+          [0, 4, 8], [2, 4, 6]
+
+        ];
+        for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+          let firstIndex = WIN_CONDITIONS[i][0];
+          let secondIndex = WIN_CONDITIONS[i][1];
+          let thirdIndex = WIN_CONDITIONS[i][2];
+          if(this.content[firstIndex] == this.content[secondIndex] &&
+                  this.content[firstIndex] == this.content[thirdIndex] &&
+                  this.content[firstIndex] != "") {
+            this.isOver = true;
+            this.winner = this.content[firstIndex];
+          }
+        }
+      },
+      calculateTie(){
+        for (let i = 0 ; i<= 8 ; i++) {
+          if(this.content[i] == "") {
+            return
+          }
+        }
+        this.isTie = true
+      },
+    }
   }
 </script>
 
